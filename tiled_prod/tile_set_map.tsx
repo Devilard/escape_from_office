d97248324d8eb37078aae27c3632b1d09fe7b3c9
Dangozzzz <94177642+Dangozzzz@users.noreply.github.com>
2025-01-08 07:19:43 +0300

    fix new map

--- tiled_prod/tile_set_map.tsx
+++ tiled_prod/tile_set_map.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="tile_set_map" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="../layouts/img/tile_map.png" width="256" height="256"/>
+<tileset version="1.10" tiledversion="1.11.0" name="tile_set_map" tilewidth="32" tileheight="32" tilecount="128" columns="16">
+ <grid orientation="orthogonal" width="64" height="64"/>
+ <image source="../layouts/img/tile_map.png" width="512" height="256"/>
 </tileset>
---
+++ tiled_prod/tile_set_map2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tile_set_map2" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../layouts/img/tile_map2.png" width="256" height="256"/>
+</tileset>
