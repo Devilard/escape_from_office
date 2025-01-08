da9bbfa8134157b2b8f0d0c26f8b36784f18563d
Dangozzzz <94177642+Dangozzzz@users.noreply.github.com>
2025-01-08 15:48:10 +0300

    mini title + fix bug + bug x 5

--- tiled_prod/tile_set_map.tsx
+++ tiled_prod/tile_set_map.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="tile_set_map" tilewidth="32" tileheight="32" tilecount="128" columns="16">
- <grid orientation="orthogonal" width="64" height="64"/>
- <image source="../layouts/img/tile_map.png" width="512" height="256"/>
+<tileset version="1.10" tiledversion="1.11.0" name="tile_set_map" tilewidth="32" tileheight="32" tilecount="256" columns="32">
+ <grid orientation="orthogonal" width="650" height="650"/>
+ <image source="../layouts/img/tile_map.png" width="1024" height="256"/>
 </tileset>
